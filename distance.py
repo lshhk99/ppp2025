857e2a6ce49085c262784af7fea4f5432f0303f9
lshhk99 <118123355+lshhk99@users.noreply.github.com>
2025-03-19 17:52:04 +0900

    Delete distance.py

--- distance.py
+++
@@ -1,9 +0,0 @@
-x_1=int(input("x_1의 좌표는?"))
-y_1=int(input("y_1의 좌표는?"))
-x_2=int(input("x_2의 좌표는?"))
-y_2=int(input("y_2의 좌표는?"))
-import math
-distance_x = abs(x_2-x_1)
-distance_y = abs(y_2-y_1)
-distance_l = math.sqrt(math.pow(distance_x,2)+math.pow(distance_y,2))
-print("좌표({}, {})와 좌표({}, {})의 거리는 {}이다".format(x_1, y_1, x_2, y_2, distance_l))
